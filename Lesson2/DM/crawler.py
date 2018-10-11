d95213740f5b61fc0cc62214e0978821769b91d9
Thomas Binetruy <thomas.binetruy@telecom-paristech.fr>
2018-10-11 17:00:59 +0200

    fetched html into string

--- Lesson2/DM/crawler.py
+++ Lesson2/DM/crawler.py
@@ -1,1 +1,13 @@
 # Crawler v1
+from bs4 import BeautifulSoup as bs
+
+import urllib
+
+url = "http://www.google.com"
+data = urllib.request.urlopen(url)
+html = ""
+for line in data.readlines():
+    html += line.__str__()
+
+print(html)
+
